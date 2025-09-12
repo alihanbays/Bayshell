d322c6a417c21c78c1d5a02e4784e60a06da3302
Alihan Baysal <baysal.alihan@outlook.com>
2025-09-12 16:29:00 -0600

    Added local tester

--- your_program.sh
+++ your_program.sh
@@ -14,8 +14,8 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
+  mkdir -p build
+  g++ -std=c++20 -O2 -Wall -Wextra  -o build/shell src/*.cpp
 )
 
 # Copied from .codecrafters/run.sh
